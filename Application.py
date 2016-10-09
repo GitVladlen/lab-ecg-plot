fa9aef6b664dbfc91f15d0c1bdefaf2c024152bc
Vladlen <vladlen.fedorenko@gmail.com>
2016-10-09 15:14:21 +0300

    Modify plotting algorithm

--- Application.py
+++ Application.py
@@ -27,21 +27,27 @@ class Application(Frame):
         control_frame = Frame(parent, padding="3 3 3 3")
         control_frame.pack(side=BOTTOM, fill=BOTH, expand=True)
 
-        label = Label(control_frame, text="Load ECG data from file (*.json): ")
-        label.grid(row=0)
-
         openfile = Button(control_frame, text="Open", command=self.on_load)
         openfile.bind("<Return>", lambda event: self.on_load())
-        openfile.grid(row=0, column=1)
+        openfile.grid(row=0)
         openfile.focus_set()
 
         savefile = Button(control_frame, text="Save", command=self.on_save)
         savefile.bind("<Return>", lambda event: self.on_save())
-        savefile.grid(row=0, column=2)
+        savefile.grid(row=0, column=1)
+
+        label = Label(control_frame, text="F:")
+        label.grid(row=0, column=2)
+
+        self.var_f = StringVar()
+        self.var_f.set(60)
+
+        ent = Entry(control_frame, textvariable=self.var_f, width=4)
+        ent.grid(row=0, column=3)
 
         submit = Button(control_frame, text="Plot ECG", command=self.on_submit)
         submit.bind("<Return>", lambda event: self.on_submit())
-        submit.grid(row=0, column=3)
+        submit.grid(row=0, column=4)
 
         for child in control_frame.winfo_children():
             child.grid_configure(padx=3, pady=3)
@@ -118,7 +124,8 @@ class Application(Frame):
         pass
 
     def on_submit(self):
-        self.plot_ecg(70)
+        F = int(self.var_f.get())
+        self.plot_ecg(F)
         pass
 
     def on_close(self):
@@ -126,71 +133,35 @@ class Application(Frame):
         pass
 
     def plot_ecg(self, F):
+        plt.close()
+
         data = self.getDataDict()
 
-        t0 = 0
-        t_wave = {}
-        for wave in self.rows:
-            mu = data[wave]["mu"]
-            b1 = data[wave]["b1"]
-            b2 = data[wave]["b2"]
-
-            t1i = mu - 3 * b1
-            t2i = mu + 3 * b2
-            t_wave[wave] = (t1i, t2i)
-
-            t0 += t2i - t1i
-            pass
-
-        t = np.arange(-50, t0+50, 0.1)
-
-        def get_wave(ti):
-            prev_ti = 0
-            for wave in self.rows:
-                t1i, t2i = t_wave[wave]
-
-                t1i += prev_ti
-                t2i += prev_ti
-
-                if t1i <= ti < t2i:
-                    return wave, t1i
-                    pass
-
-                prev_ti = t2i
-                pass
-
-            return None, None
-            pass
+        coef = (60 * 1000) / F
 
         def func(ti):
-            wave, t_start = get_wave(ti)
+            sum = 0
+            for wave in self.rows:
+                A = data[wave]["A"] * coef
+                mu = data[wave]["mu"] * coef
+                b = data[wave]["b1" if ti <= mu else "b2"]  * coef
 
-            if wave is None:
-                return 0
-                pass
+                numerator = (ti-mu)**2
+                denominator = -2 * b**2
+                sum += A * math.exp(numerator/denominator)
+            return sum
 
-            t_rel = ti - t_start
+        t = np.arange(0, coef, 1)
+        y = [func(ti) for ti in t]
+       
+        offset = 0
 
-            A = data[wave]["A"]
-            mu = data[wave]["mu"]
-            b1 = data[wave]["b1"]
-            b2 = data[wave]["b2"]
-
-            if t_rel <= mu:
-                b = b1
-            else:
-                b = b2
-
-            return A * math.exp(-((t_rel-mu)**2)/(2*b**2))
+        for i in range(5):
+            x = [ti + offset for ti in t]
+            plt.plot(x, y)
+            offset += coef
             pass
 
-        y = [func(ti) for ti in t]
-
-        ax = plt.axes()
-        ax.axes.get_xaxis().set_visible(False)
-        ax.axes.get_yaxis().set_visible(False)
-
-        plt.plot(t, y)
         plt.title("ECG")
         plt.show()
         pass
